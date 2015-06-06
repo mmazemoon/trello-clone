99845f5c6eb86b8edee276f162be2d9e44e4f0fa
Mark Azemoon <mmazemoon@gmail.com>
2015-06-05 18:56:53 -0700

    Build getOrFetch function

--- app/assets/javascripts/collections/boards.js
+++ app/assets/javascripts/collections/boards.js
@@ -3,6 +3,20 @@ TrelloClone.Collections.Boards = Backbone.Collection.extend({
   model: TrelloClone.Models.Board,
   url: "/api/boards",
 
-  
+  getOrFetch: function(id){
+    var board = this.get("id");
+    var collection = this;
+    if(board){
+      board.fetch();
+    }else{
+      board = new TrelloClone.Models.Board({"id": id});
+      board.fetch({
+        success: function(){
+          collection.add(board);
+        }
+      });
+    }
+    return board;
+  }
 
 });
