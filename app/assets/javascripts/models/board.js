99cf6bc92d39379a4fe7d1e3d8d08bfee1b780c2
Mark Azemoon <mmazemoon@gmail.com>
2015-06-05 18:58:28 -0700

    Implement parse and lists function to accomodate JSON response

--- app/assets/javascripts/models/board.js
+++ app/assets/javascripts/models/board.js
@@ -1,5 +1,20 @@
 TrelloClone.Models.Board = Backbone.Model.extend({
 
-  urlRoot: "/api/boards"
+  urlRoot: "/api/boards",
+
+  parse: function(response){
+    if (response.lists) {
+      this.lists().set(response.lists);
+      delete response.lists;   // delete vanilla javascript operator, e.g. +, -
+    }
+    return response;  // always return response.
+  },
+
+  lists: function (){
+    if (!this._lists){
+      this._lists = new TrelloClone.Collections.Lists();
+    }
+    return this._lists; // always return this._lists
+  }
 
 });
