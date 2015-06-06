39ded7f6e11e2c985c608a1707c773d3c10b7056
Mark Azemoon <mmazemoon@gmail.com>
2015-06-05 18:54:49 -0700

    Implement boardShow route/function

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,11 +1,18 @@
 TrelloClone.Routers.Router = Backbone.Router.extend({
   routes: {
-    "": "boardIndex"
+    "": "boardIndex",
+    "boards/:id": "boardShow"
   },
 
   boardIndex: function() {
     this.boards.fetch();
     var view = new TrelloClone.Views.BoardsIndex({ collection: this.boards });
+    this.$rootEl.html(view.render().$el);
+  },
+
+  boardShow: function(id) {
+    var board = this.boards.getOrFetch(id);
+    var view = new TrelloClone.Views.BoardShow({ model: board });
     this.$rootEl.html(view.render().$el);
   },
 
